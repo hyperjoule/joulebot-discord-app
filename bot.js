79eead88c868e791e6749b9a67b517543c6783a3
Juliann Stainbrook <hyperjoule@gmail.com>
2023-04-29 21:05:43 -0400

    Refactor a bit for clarity

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const { Client, GatewayIntentBits, EmbedBuilder} = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { handleSend, generateImage } = require('./chatbot_api');
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
 const { personalityTitles } = require('./personalities');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 let selectedPersonalityIdx = 0;
 
@@ -48,11 +48,40 @@ client.on('ready', async () => {
 					choices: personalityTitles.map(title => ({ name: title.label, value: title.value }))
 				}
 			]
-		}	
+		}
 	]);
 
 	console.log('Registered slash commands:', commands);
 });
+
+async function handleAskCommand(interaction) {
+	await interaction.deferReply();
+	const userName = interaction.member.displayName;
+	const userInput = interaction.options.getString('question');
+	const chatbotResponse = await handleSend(userInput, selectedPersonalityIdx);
+	await interaction.editReply(`**${userName} asks:** ${userInput}\n\n**Joulebot:** ${chatbotResponse}`);
+}
+
+async function handleDrawCommand(interaction) {
+	await interaction.deferReply();
+	const userName = interaction.member.displayName;
+	const imageDescription = interaction.options.getString('description');
+	const imageUrl = await generateImage(imageDescription);
+	if (imageUrl) {
+		const imageEmbed = new EmbedBuilder()
+			.setColor('#0099ff')
+			.setTitle(`**${userName} requested:** ${imageDescription}`)
+			.setImage(imageUrl);
+		await interaction.editReply({ embeds: [imageEmbed] });
+	} else {
+		await interaction.editReply("I'm sorry, but I'm having trouble generating an image right now. Please try again later.");
+	}
+}
+
+async function handlePersonalityCommand(interaction) {
+	selectedPersonalityIdx = parseInt(interaction.options.getString('choice'));
+	await interaction.reply(`Personality set to: ${personalityTitles[selectedPersonalityIdx].label}`);
+}
 
 client.on('interactionCreate', async interaction => {
 	console.log('Interaction received:', interaction.commandName);
@@ -60,33 +89,15 @@ client.on('interactionCreate', async interaction => {
 
 	switch (interaction.commandName) {
 	case 'ask':
-		await interaction.deferReply();
-		const userName = interaction.user.username; 
-		const userInput = interaction.options.getString('question');
-		imageTitle = userInput;
-		const chatbotResponse = await handleSend(`**${userName} asks:** ${userInput}`, selectedPersonalityIdx);
-		await interaction.editReply(`**${userName} asks:** ${userInput}\n\n**Joulebot:** ${chatbotResponse}`);
-		break;
+		await handleAskCommand(interaction);
+		break;   
 	case 'draw':
-		await interaction.deferReply();
-		const imageDescription = interaction.options.getString('description');
-		const imageUrl = await generateImage(imageDescription);
-		if (imageUrl) {
-			const imageEmbed = new EmbedBuilder ()
-				.setColor('#0099ff')
-				.setTitle(`**${interaction.user.username} requested:** ` + interaction.options.getString('description'))
-				.setImage(imageUrl);
-			await interaction.editReply({ embeds: [imageEmbed] });
-		} else {
-			await interaction.editReply("I'm sorry, but I'm having trouble generating an image right now. Please try again later.");
-		}
+		await handleDrawCommand(interaction);
 		break;
 	case 'personality':
-		// Update the selected personality index based on user input
-		selectedPersonalityIdx = parseInt(interaction.options.getString('choice'));
-		await interaction.reply(`Personality set to: ${personalityTitles[selectedPersonalityIdx].label}`);
+		await handlePersonalityCommand(interaction);
 		break;
 	}
 });
-
+				
 client.login(process.env.BOT_TOKEN);
